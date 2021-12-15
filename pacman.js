68dd616a65dcbc9d5b27a43d0bb61a787026c6ce
Roman Ibba <romanibba95@gmail.com>
2021-12-14 18:25:42 -0800

    added img folders

--- pacman.js
+++ pacman.js
@@ -951,16 +951,16 @@ var PACMAN = (function() {
                     setState(EATEN_PAUSE);
                     timerStart = tick;
                     if (eatenCount == 2) {
-                        document.getElementById("myText").innerHTML = "<p><img src=\"pacStackNew.PNG\" height=\"250px\" width=\"400px\"></p>";
+                        document.getElementById("myText").innerHTML = "<p><img src=\"img/pacStackNew.PNG\" height=\"250px\" width=\"400px\"></p>";
                     }
                     if (eatenCount == 4) {
-                        document.getElementById("myText2").innerHTML = "<p><img src=\"pacHobbies.PNG\" height=\"250px\" width=\"400px\"></p>";
+                        document.getElementById("myText2").innerHTML = "<p><img src=\"img/pacHobbies.PNG\" height=\"250px\" width=\"400px\"></p>";
                     }
                     if (eatenCount == 6) {
-                        document.getElementById("myText3").innerHTML = "<p><img src=\"pacEducationNew2.PNG\" height=\"250px\" width=\"400px\"></p>";
+                        document.getElementById("myText3").innerHTML = "<p><img src=\"img/pacEducationNew2.PNG\" height=\"250px\" width=\"400px\"></p>";
                     }
                     if (eatenCount == 8) {
-                        document.getElementById("myText4").innerHTML = "<p><img src=\"pacExperienceNew.PNG\" height=\"250px\" width=\"400px\"></p>";
+                        document.getElementById("myText4").innerHTML = "<p><img src=\"img/pacExperienceNew.PNG\" height=\"250px\" width=\"400px\"></p>";
                     }
                     // document.getElementById("myText").innerHTML = "<p><img src=\"https://www.njea.org/wp-content/uploads/2014/10/Coding.jpg\" height=\"250px\" width=\"400px\"></p>"; //  sends eaten ghost count to html page
                 } else if (ghosts[i].isDangerous()) {
